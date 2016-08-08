91c318d3bbb3f627e5bde69a9a8c1fe33e255e75
Manisenthil <sm98667@gmail.com>
2016-08-08 01:45:57 -0700

    Create Reversed.java

---
+++ Reversed.java
@@ -0,0 +1,15 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+class Reversed
+{
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the character:");
+	String a=s.nextLine();
+	StringBuffer b=new StringBuffer(a);
+	b.reverse();
+	a=b.toString();
+	System.out.println(a);
+	
+	}
+}
