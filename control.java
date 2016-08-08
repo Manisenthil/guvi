21bc2ee87aed9298e1e5da4ebb451c674b944993
Manisenthil <sm98667@gmail.com>
2016-08-08 01:52:55 -0700

    Create control.java

---
+++ control.java
@@ -0,0 +1,10 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+class control
+{
+	public static void main(String[] args) {
+	String str="123344";
+	int a=Integer.parseInt(str);
+	System.out.println(a);
+	}
+}
